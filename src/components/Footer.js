85c01945bb631c66d66d16711e6dae80f2f6a5b7
Brien Mizell <40799762+brienmizell@users.noreply.github.com>
2019-06-18 17:01:09 -0400

    Adding new api folder to hold a json server for creating projects database.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,9 @@ const Footer = () => {
     <div className="three ui buttons">
       <div className="big ui animated fade facebook button ">
         <div class="visible content">
-          <i className="big linkedin icon" />
+          <i className="big linkedin icon">
+            <a href="http://www.linkedin.com" target="_blank" />
+          </i>
         </div>
         <div className="big hidden content">LinkedIn</div>
       </div>
