545606a8874bb0e0fd7e2d7e83f0cc94b38b3f16
Brien Mizell <40799762+brienmizell@users.noreply.github.com>
2019-06-25 19:14:43 -0400

    Added animation component to portfolio to display animation.

---
+++ src/components/Animation.js
@@ -0,0 +1,12 @@
+import React from "react";
+import App from "./projects/animatedText/src/App";
+
+const Animation = () => {
+  return (
+    <div>
+      <App />
+    </div>
+  );
+};
+
+export default Animation;
--- src/components/App.js
+++ src/components/App.js
@@ -11,6 +11,7 @@ import Footer from "./Footer";
 import MusicApp from "./MusicApp";
 import Pictures from "./Pictures";
 import DateCountdown from "./DateCountdown";
+import Animation from "./Animation";
 
 const App = () => {
   return (
@@ -25,6 +26,7 @@ const App = () => {
           <Route path="/music" exact component={MusicApp} />
           <Route path="/pictures" exact component={Pictures} />
           <Route path="/countdown" exact component={DateCountdown} />
+          <Route path="/animation" exact component={Animation} />
         </Switch>
         <Footer />
       </Router>
