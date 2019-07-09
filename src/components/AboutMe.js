1b42b83382b11091805918cc7cfb968f4a5c4e4b
Brien Mizell <40799762+brienmizell@users.noreply.github.com>
2019-07-08 21:50:58 -0400

    Added to about me and added a new font.

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -6,26 +6,36 @@ const AboutMe = () => {
   return (
     <div id="about-me">
       <AboutTop />
-      <h1>Hi, that guy above is me.</h1>
+      <h1>Hi, that guy above is me, Brien Mizell.</h1>
       <p>Why did I want to become a Software Engineer?</p>
       <p>
-        The simple answer is that I love it. I love the thrill. I like to see my
-        imagination come to life, and I love gadgets. I choose to be a Software
-        Engineer to bring my ideas to life and help others understand what I
-        think a better world looks like.
+        The simple answer is that I love it. I love the thrill, and I love
+        gadgets. I choose to be a Software Engineer to bring my ideas to life
+        and help others understand how I think a better world looks.
       </p>
       <p>
-        I have always been a programmer, per se. I always enjoyed programming my
-        life and making edits so that my life would run as smoothly and
-        efficiently as possible. Once I was introduced to computers, I would
-        love searching for ways they could make my life simpler or more
-        enriched. I am now writing those apps to help others live a better life
-        and be better connected to the world around them.
+        In a way, I have always been a programmer, per se. I always enjoyed
+        programming my life and making edits so that my life would run smoothly
+        and implementing life-hacks so that it could be as efficient as
+        possible. Once introduced to computers, I would love searching for ways
+        they could make my life simpler or more enriched. I am now writing those
+        apps to help others live a better life and be more connected to the
+        world around them.
       </p>
       <p>
-        I went from programming CNC machines for cutting out precision parts to
+        I started programming CNC machines for cutting out precision parts to
         build tangible things, and I now use programming languages to help bring
         ideas to fruition in the virtual world.
+      </p>
+      <p>
+        I love spending time with my dog, eating good food, spending time with
+        great friends, drinking good beer, listening to good music, and watching
+        sports. I'm a Georgia native, and I love everything <span>�</span>.
+      </p>
+      <p>
+        I take pride in the work I do and want to work for a company that will
+        take the same pride in their employees. I don't shy away from hard work
+        and long hours, because any job worth doing is worth doing right.{" "}
       </p>
     </div>
   );
