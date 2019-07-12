47206f089818f57c392ed6ab439a33bf887e60b5
Brien Mizell <40799762+brienmizell@users.noreply.github.com>
2019-07-12 18:55:13 -0400

    Adding skills component to portfolio for adding icons of languages, etc I know.

---
+++ src/components/Skills.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+const Skills = () => {
+  return (
+    <div>
+      <i
+        className=" huge js icon"
+        style={{
+          color: "#f0db4f"
+        }}
+      />
+      <i
+        className=" huge react icon"
+        style={{
+          color: "#61dbfb"
+        }}
+      />
+      <i
+        className=" huge node icon"
+        style={{
+          color: "yellow"
+        }}
+      />
+    </div>
+  );
+};
+
+export default Skills;
