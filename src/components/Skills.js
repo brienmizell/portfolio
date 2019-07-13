d5bf2d126e13b2ee2c8118327a6829b13c98d3a2
Brien Mizell <40799762+brienmizell@users.noreply.github.com>
2019-07-13 17:12:55 -0400

    Changes styling on skills component.

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -4,41 +4,98 @@ import "./Skills.css";
 const Skills = () => {
   return (
     <div
-      className="ui 5 item"
-      //   style={{
-      //     // backgroundColor: "black",
-      //     display: "flex",
-
-      //     justifyContent: "space-around",
-      //     height: "auto"
-      //   }}
+      className="ui grid container cards"
+    //   style={{
+    //     // backgroundColor: "black",
+    //     display: "flex",
+    //     flexWrap: "wrap",
+    //     justifyContent: "space-around",
+    //     height: "auto"
+    //   }}
     >
-      <img src={require("../images/html5.svg")} style={{ height: "80px" }} />
+        <h1 style={{paddingTop: "25px", width:"100%"}}>Skills</h1>
+      <div className="four wide column">
+<img src={require("../images/html5.svg")} style={{ height: "80px" }} alt="icon" />
+      
+          </div>
+      
 
-      <img src={require("../images/Js.svg")} style={{ height: "80px" }} />
-      <img src={require("../images/React.svg")} style={{ height: "80px" }} />
+      <div className="four wide column">
+          <img src={require("../images/Js.svg")} style={{ height: "80px" }} alt="icon" />
+        </div>
 
-      <img
-        src={require("../images/nodejs.svg")}
-        style={{ height: "80px", height: "80px" }}
-      />
-      <img src={require("../images/Python.svg")} style={{ height: "80px" }} />
-      <img src={require("../images/AJAX.svg")} style={{ height: "80px" }} />
-      <img
-        src={require("../images/expressjs.svg")}
-        style={{ height: "80px", color: "white" }}
-      />
-      <img src={require("../images/Angular.svg")} style={{ height: "80px" }} />
-      <img src={require("../images/Redux.svg")} style={{ height: "80px" }} />
-      <img
-        src={require("../images/postgresql.svg")}
-        style={{ height: "80px", color: "#336791" }}
-      />
-      <img src={require("../images/slack.svg")} style={{ height: "80px" }} />
-      <img
-        src={require("../images/raspberry-pi.svg")}
-        style={{ height: "80px" }}
-      />
+
+<div className="four wide column">
+
+      <img src={require("../images/React.svg")} style={{ height: "80px" }} alt="icon" />
+
+</div>
+
+    <div className="four wide column">
+        <img
+            src={require("../images/nodejs.svg")}
+            style={{ height: "80px", height: "80px" }}
+            alt="icon"
+        />
+    </div>
+    <div className="four wide column">
+
+        <img src={require("../images/Python.svg")} style={{ height: "80px" }} alt="icon"/>
+
+    </div>
+    
+    <div className="four wide column">
+
+        <img src={require("../images/AJAX.svg")} style={{ height: "80px" }} alt="icon" />
+
+    </div>
+    
+    <div className="four wide column">
+
+        <img
+            src={require("../images/expressjs.svg")}
+            style={{ height: "80px", color: "white" }}
+            alt="icon"
+        />
+    </div>
+
+    <div className="four wide column">
+
+        <img src={require("../images/Angular.svg")} style={{ height: "80px" }} alt="icon" />
+
+    </div>
+    
+    <div className="four wide column">
+
+        <img src={require("../images/Redux.svg")} style={{ height: "80px" }} alt="icon"/>
+
+    </div>
+    
+    <div className="four wide column">
+
+        <img
+            src={require("../images/postgresql.svg")}
+            style={{ height: "80px", color: "#336791" }}
+            alt="icon"
+
+    />
+    </div>
+
+    <div className="four wide column">
+
+        <img src={require("../images/slack.svg")} style={{ height: "80px" }} alt="icon" />
+
+    </div>
+    
+    <div className="four wide column">
+
+        <img
+            src={require("../images/raspberry-pi.svg")}
+            style={{ height: "80px" }}
+            alt="icon"
+        />
+
+    </div>
     </div>
   );
 };
