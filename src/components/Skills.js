205a00d025bcecc650b7dc22f8eeb606cbce9b06
Brien Mizell <40799762+brienmizell@users.noreply.github.com>
2019-07-13 16:32:44 -0400

    Added skills component and images.

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,25 +1,43 @@
 import React from "react";
+import "./Skills.css";
 
 const Skills = () => {
   return (
-    <div>
-      <i
-        className=" huge js icon"
-        style={{
-          color: "#f0db4f"
-        }}
+    <div
+      className="ui 5 item"
+      //   style={{
+      //     // backgroundColor: "black",
+      //     display: "flex",
+
+      //     justifyContent: "space-around",
+      //     height: "auto"
+      //   }}
+    >
+      <img src={require("../images/html5.svg")} style={{ height: "80px" }} />
+
+      <img src={require("../images/Js.svg")} style={{ height: "80px" }} />
+      <img src={require("../images/React.svg")} style={{ height: "80px" }} />
+
+      <img
+        src={require("../images/nodejs.svg")}
+        style={{ height: "80px", height: "80px" }}
       />
-      <i
-        className=" huge react icon"
-        style={{
-          color: "#61dbfb"
-        }}
+      <img src={require("../images/Python.svg")} style={{ height: "80px" }} />
+      <img src={require("../images/AJAX.svg")} style={{ height: "80px" }} />
+      <img
+        src={require("../images/expressjs.svg")}
+        style={{ height: "80px", color: "white" }}
       />
-      <i
-        className=" huge node icon"
-        style={{
-          color: "yellow"
-        }}
+      <img src={require("../images/Angular.svg")} style={{ height: "80px" }} />
+      <img src={require("../images/Redux.svg")} style={{ height: "80px" }} />
+      <img
+        src={require("../images/postgresql.svg")}
+        style={{ height: "80px", color: "#336791" }}
+      />
+      <img src={require("../images/slack.svg")} style={{ height: "80px" }} />
+      <img
+        src={require("../images/raspberry-pi.svg")}
+        style={{ height: "80px" }}
       />
     </div>
   );
